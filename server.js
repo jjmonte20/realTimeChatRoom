8672a625f51bb7e4ddcf199189db2b716d89d2b4
jjmonte20 <jjmonte20@gmail.com>
2019-02-02 14:10:04 -0600

    able to make socket connection, added boilerplate data

---
+++ server.js
@@ -0,0 +1,26 @@
+// welcome to the boiler plate js file for making a full stack website
+// this will cause a lot less friction when connecting a page to the web
+// since all of the info is on the page
+var express = require("express");
+var socket = require('socket.io');
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+
+
+// always put on the bottom of the page, listen page will always be active in a given web app
+var server = app.listen(PORT, function() {
+    console.log(PORT + " is listening")
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+    console.log('made socket connection', socket.id);
+})
