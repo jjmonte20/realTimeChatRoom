15ac342b95f70f305c8a96f1d5fcc774fae376b3
jjmonte20 <jjmonte20@gmail.com>
2019-02-02 14:44:49 -0600

    fixed typo in html causing socket to break

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,22 @@
 // Make connection
 var socket = io.connect('http://localhost:8080');
+
+// Query DOM
+var message = document.getElementById('message'),
+      handle = document.getElementById('handle'),
+      btn = document.getElementById('send'),
+      output = document.getElementById('output');
+
+// emit events
+btn.addEventListener('click', function(){
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
+    });
+    message.value = '';
+});
+
+// listen for events
+socket.on('chat', function(data){
+    output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
+});
